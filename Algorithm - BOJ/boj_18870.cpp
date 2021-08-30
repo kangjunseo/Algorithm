ff07c963d751a13e384bc33240a9b6e7cda7a394
KJS <88201041+kangjunseo@users.noreply.github.com>
2021-08-30 15:20:53 +0900

    Create boj_18870.cpp

---
+++ Algorithm - BOJ/boj_18870.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+bool comp(pair<double,int> a, pair<double,int> b){
+  return a.first < b.first;
+}
+
+bool comp2(pair<double,int> a, pair<double,int> b){
+  return a.second < b.second;
+}
+
+int main() {
+  int N;
+  cin>>N;
+  vector <pair<double,int>> v;
+  for(int i=0;i<N;i++){
+    double temp;
+    cin>>temp;
+    v.push_back(make_pair(temp,i));
+  }
+    
+  sort(v.begin(),v.end(),comp);
+  int idx =-1;
+  double previous=-1e9-1;
+  for(auto iter = v.begin();iter!=v.end();++iter){
+    if((*iter).first==previous)
+      (*iter).first=idx;
+    else{
+      previous=(*iter).first;
+      (*iter).first=++idx;
+    }
+  }
+  
+  sort(v.begin(),v.end(),comp2);
+  for(auto iter = v.begin();iter!=v.end() ;++iter){
+    cout<<(*iter).first<<' ';
+  }
+}
